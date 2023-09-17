391fe034183fad46550f1b0c084bd635493d6fe0
Chaichontat Sriworarat <csriwor1@jh.edu>
2023-09-17 18:47:04 +0000

    restriction filter
    Signed-off-by: Chaichontat Sriworarat <csriwor1@jh.edu>

--- fishtools/mkprobes/screen.py
+++ fishtools/mkprobes/screen.py
@@ -1,8 +1,11 @@
 import sys
 from pathlib import Path
+from typing import overload
 
 import click
 import polars as pl
+from Bio import Restriction
+from Bio.Seq import Seq
 from loguru import logger
 
 from .utils._filtration import the_filter
@@ -11,14 +14,16 @@ from .utils.samframe import SAMFrame
 sys.setrecursionlimit(5000)
 
 
-def run_screen(
+def _screen(
     data_dir: str | Path,
     gene: str,
     fpkm_path: str | Path | None = None,
     overlap: int = -2,
+    restriction: list[str] | None = None,
 ):
     data_dir = Path(data_dir)
-    ff = SAMFrame(pl.read_parquet(data_dir / f"{gene}_crawled.parquet"))
+
+    ff = SAMFrame(pl.read_parquet(next(data_dir.glob(f"{gene}*_crawled.parquet"))))
 
     if fpkm_path is not None:
         fpkm = pl.read_parquet(fpkm_path)
@@ -26,13 +31,73 @@ def run_screen(
             pl.col("FPKM").lt(0.05 * pl.col("FPKM").first()) | pl.col("FPKM").lt(1) | pl.col("FPKM").is_null()
         )
 
+    if restriction is not None:
+        ff = ff.filter(
+            pl.col("seq").apply(
+                lambda x: any(Restriction.RestrictionBatch(restriction).search(Seq(x)).values())
+            )
+        )
+
     final = the_filter(ff, overlap=overlap)
+
     final.write_parquet(
         data_dir / f"{gene}_screened_ol{overlap}.parquet"
         if overlap > 0
         else data_dir / f"{gene}_screened.parquet"
     )
     return final
+
+
+@overload
+def run_screen(
+    data_dir: str,
+    gene: str,
+    fpkm_path: Path | str | None = ...,
+    overlap: int = ...,
+    minimum: None = ...,
+    maxoverlap: int = ...,
+    restriction: list[str] | None = ...,
+) -> pl.DataFrame:
+    ...
+
+
+@overload
+def run_screen(
+    data_dir: str,
+    gene: str,
+    fpkm_path: Path | str | None = ...,
+    overlap: int = ...,
+    minimum: int = ...,
+    maxoverlap: int = ...,
+    restriction: list[str] | None = ...,
+) -> dict[int, pl.DataFrame]:
+    ...
+
+
+def run_screen(
+    data_dir: str,
+    gene: str,
+    fpkm_path: Path | str | None = None,
+    overlap: int = -2,
+    minimum: int | None = None,
+    maxoverlap: int = 20,
+    restriction: list[str] | None = None,
+) -> dict[int, pl.DataFrame] | pl.DataFrame:
+    if minimum is not None:
+        if maxoverlap % 5 != 0 or maxoverlap == 0:
+            raise ValueError("maxoverlap must be positive non-zero and a multiple of 5")
+
+        res = {}
+        for i in range(0, maxoverlap + 1, 5):
+            res[i] = (final := _screen(data_dir, gene, fpkm_path=fpkm_path, overlap=i))
+            if len(final) >= minimum:
+                logger.info(f"Overlap {i} results in {len(final)} probes. Stopping.")
+                return res
+            logger.warning(f"Overlap {i} results in {len(final)} probes. Trying next overlap.")
+
+        return res
+
+    return _screen(data_dir, gene, fpkm_path=fpkm_path, overlap=overlap, restriction=restriction)
 
 
 @click.command()
@@ -50,6 +115,7 @@ def run_screen(
 @click.option(
     "--maxoverlap", type=int, default=20, help="Maximum sequence overlap between probes if minimum is set."
 )
+@click.option("--restriction", type=str, multiple=True, help="Restriction enzymes to filter probes by.")
 def screen(
     data_dir: str,
     gene: str,
@@ -57,17 +123,7 @@ def screen(
     overlap: int = -2,
     minimum: int | None = None,
     maxoverlap: int = 20,
+    restriction: list[str] | None = None,
 ):
     """Screening of probes candidates for a gene."""
-    if minimum is not None:
-        if maxoverlap % 5 != 0 or maxoverlap == 0:
-            raise ValueError("maxoverlap must be positive non-zero and a multiple of 5")
-
-        for i in range(0, maxoverlap + 1, 5):
-            final = run_screen(data_dir, gene, fpkm_path=fpkm_path, overlap=i)
-            if len(final) >= minimum:
-                logger.info(f"Overlap {i} results in {len(final)} probes. Stopping.")
-                return
-            logger.warning(f"Overlap {i} results in {len(final)} probes. Trying next overlap.")
-
-    screen(data_dir, gene, fpkm_path=fpkm_path, overlap=overlap)
+    run_screen(data_dir, gene, fpkm_path=fpkm_path, overlap=overlap, minimum=minimum, maxoverlap=maxoverlap)
