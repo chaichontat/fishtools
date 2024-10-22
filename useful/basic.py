3a3093c4cefccaaef0895e888a08c841b52ec312
Chaichontat Sriworarat <csriwor1@jh.edu>
2024-10-22 13:06:46 -0400

    basic

--- useful/basic.py
+++ useful/basic.py
@@ -32,24 +32,26 @@ def plot(basic: BaSiC):
 channels = [560, 650, 750]
 # %%
 path = Path("/mnt/archive/starmap/e155/e155_trc")
-files = list((path).rglob("3_11_19*/*.tif"))
-files += list((path).rglob("4_12_20*/*.tif"))
+round_ = "3_11_19"
+files = list((path).glob(f"{round_}*/*.tif"))
+# files += list((path).rglob("4_12_20*/*.tif"))
 
 if len(files) < 500:
     logger.warning(f"Not enough files ({len(files)}). Result may be unreliable.")
 
-n = 500
-z = 8
+n = min(len(files), 500)
+zs = [10, 20]
 c = len(channels)
 
 # This is the extracted z slices from all files.
-out = np.zeros((n, c, 2048, 2048), dtype=np.uint16)
+out = np.zeros((n * len(zs), c, 2048, 2048), dtype=np.uint16)
 for i, file in enumerate(files[:n]):
-    if i % 100 == 0:
+    if i % 100 / len(channels) == 0:
         logger.info("Loaded {}/{}", i, n)
     with TiffFile(file) as tif:
         for j in range(c):
-            out[i, j] = tif.pages[z * c + j].asarray()
+            for k, z in enumerate(zs):
+                out[i * len(channels) + k, j] = tif.pages[z * c + j].asarray()
 
 logger.info(f"Loaded {len(files)} files. {out.shape}")
 # img = BaSiC(get_darkfield=True, smoothness_flatfield=1)(img)
@@ -62,17 +64,16 @@ with jax.default_device(jax.devices("cpu")[0]):
     for c in range(out.shape[1]):
         logger.info("Fitting channel {}", c)
         basic = BaSiC()
-        basic.fit(out[: len(files), c])
+        basic.fit(out[:, c])
         basics.append(basic)
         plot(basics[c])
         plt.show()
 # %%
 for b, c in zip(basics, channels):
-    with open(path / f"basic_{c}.pkl", "wb") as f:
+    with open(path / f"basic_{round_}_{c}.pkl", "wb") as f:
         pickle.dump(b, f)
 # %%
-with open(path / f"basic_650.pkl", "rb") as f:
-    x = pickle.load(f)
+
 # %%
 plot(x)
 # %%
